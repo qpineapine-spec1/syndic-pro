e5370852227d72d6de6a1d69bbcb9e132096fb2c
qpineapine-spec1 <qpineapine@gmail.com>
2026-08-14 08:29:04 +0100

    Configure middleware to trust proxies
    Added trustProxies middleware configuration to handle forwarded headers.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,9 @@
 <?php
-
 use App\Http\Middleware\EnsureUserHasRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => EnsureUserHasRole::class,
         ]);
+
+        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
